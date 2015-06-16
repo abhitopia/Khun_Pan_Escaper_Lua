36d4a6468684aeede3f1fc526a0724bd2d58f9c2
Abhi <abhishek.aggarwal@adform.com>
2015-06-16 13:40:49 +0100

    added toggle feature and fixed bugs

--- game.lua
+++ game.lua
@@ -13,6 +13,7 @@ Objective:
 
 local curses = require 'curses'
 local posix  = require 'posix'
+local inspect = require 'inspect'
 
 function copy(obj, seen)
   	if type(obj) ~= 'table' then return obj end
@@ -88,6 +89,8 @@ function Game:init()
   	for k, v in ipairs(self.sentries) do
     		c_fg, c_bg = curses['COLOR_' .. v.fg_color:upper()], curses['COLOR_' .. v.bg_color:upper()]
     		curses.init_pair(k, c_fg, c_bg)
+		--curses.init_pair(k+33, c_bg, c_fg)
+		v.color = k
   	end
 	local border = self.border
 	
@@ -100,6 +103,10 @@ function Game:init()
 
 	function draw_point(x, y, x_offset, y_offset, color, point_char)
   		point_char = point_char or ' '  -- Space is the default point_char.
+		if color == 0 then 
+			--print('found empty', x, y)
+			color = 12 
+		end
   		if color then set_color(color) end
   		-- Don't draw pieces when the game is paused.
 		y_scale, x_scale = self.y_scale, self.x_scale
@@ -109,8 +116,10 @@ function Game:init()
 				stdscr:mvaddstr(y + dy, x + dx, point_char)
 			end
 		end
-		set_color(12)
+		--set_color(12)
 	end 
+	
+	self.active_sentry = 0
 end
 
 function Game:handle_input()
@@ -121,53 +130,52 @@ function Game:handle_input()
     		curses.endwin()
     		os.exit(0)
 	elseif  key == curses.KEY_DOWN then
-		self:move(0,1)
+		self:move('down')
 	elseif  key == curses.KEY_LEFT then
-		self:move(-1,0)
+		self:move('left')
 	elseif  key == curses.KEY_RIGHT then
-		self:move(1,0)
+		self:move('right')
 	elseif key == curses.KEY_UP then
-		self:move(0,-1)	
+		self:move('up')
+	elseif key == tostring(' '):byte(1) then
+		self:toggle_active_sentry()	
     	end
 end
 
 
 function Game:set_sentry(sentry, color, char)
-	for dx=0,sentry.dx-1 do
+	for dx=0, sentry.dx-1 do
 		for dy=0, sentry.dy-1 do 
-			self.board_color[sentry.y+dy][sentry.x+dx] = color or sentry.fg_color
+			self.board_color[sentry.y+dy][sentry.x+dx] = color or sentry.color
 			self.board_char[sentry.y+dy][sentry.x+dx] = char or sentry.char
 		end
 	end
 end
 
-function Game:check_valid_sentry(sentry)
+function Game:is_valid_move(old_sentry, new_sentry)
+	self:set_sentry(old_sentry, 0, ' ')
 	valid = true
-	for dx=0,sentry.dx-1 do
-		for dy=0, sentry.dy-1 do 
-			if self.board_color[sentry.y+dy][sentry.x+dx] ~= 0 or 
-				self.board_char[sentry.y+dy][sentry.x+dx] ~= ' ' then
+	for dx=0, new_sentry.dx-1 do
+		for dy=0, new_sentry.dy-1 do 
+			if self.board_color[new_sentry.y+dy][new_sentry.x+dx] ~= 0 then
 				valid = false
+				break
 			end
 		end
 	end
+	self:set_sentry(old_sentry)
 	return valid
 end
 
-function Game:move(dx, dy)
-	local sentries =  self.sentries
-	for i, sentry in ipairs(sentries) do
-		local board_color = self.board_color
+function Game:move(direction)
+	sentry =  self.sentries[self.active_sentry]
+	new_sentry = self:get_moved_sentry(sentry, direction)
+	if self:is_valid_move(sentry, new_sentry) then
 		self:set_sentry(sentry, 0, ' ')
-		local new_sentry = copy(sentry)
-		new_sentry.x, new_sentry.y = new_sentry.x + dx, new_sentry.y + dy
-		if self:check_valid_sentry(new_sentry) then
-			self.sentries[i] = new_sentry
-			self:set_sentry(self.sentries[i])
-			break
-		end
-		self:set_sentry(self.sentries[i])
-	end	
+		self.sentries[self.active_sentry] = new_sentry
+		self:set_sentry(self.sentries[self.active_sentry])
+	end
+	--print(inspect(self.board_color))
 end
 
 function Game:fill_board()
@@ -191,7 +199,8 @@ function Game:draw_screen()
 	local board_height = self.y_scale * 7
   	local x_margin = math.floor((scr_width - board_width) / 2)
 	local y_margin = 10	
-	
+
+	active_sentry = self.sentries[self.active_sentry] 
 	-- Draw the board's border and non-falling pieces if we're not paused.
   	for y = 1, 7 do
     		for x = 1, 6 do
@@ -201,21 +210,64 @@ function Game:draw_screen()
       			draw_point( x, y,  x_margin, y_margin, color, char)
     		end
   	end
+	
+end
+
+function Game:get_moved_sentry(sentry, direction)
+	moved_sentry = copy(sentry)
+	if direction == 'up' then
+		moved_sentry.y = moved_sentry.y - 1
+	elseif direction == 'left' then
+		moved_sentry.x = moved_sentry.x - 1
+	elseif direction == 'right' then 
+		moved_sentry.x = moved_sentry.x + 1
+	elseif direction == 'down' then
+		moved_sentry.y = moved_sentry.y + 1
+	end
+	return moved_sentry
+end
+
+function Game:update_movable_sentries()
+	for i, sentry in ipairs(self.sentries) do
+		can_up = self:is_valid_move(sentry, self:get_moved_sentry(sentry, 'up'))
+		can_down = self:is_valid_move(sentry, self:get_moved_sentry(sentry, 'down'))
+		can_left = self:is_valid_move(sentry, self:get_moved_sentry(sentry, 'left'))
+		can_right = self:is_valid_move(sentry, self:get_moved_sentry(sentry, 'right'))
+		if can_up or can_down or can_right or can_left then
+			self.sentries[i].movable = true
+		else
+			self.sentries[i].movable = false
+		end
+	end
+end
+
+function Game:toggle_active_sentry()
+	while(true) do
+		self.active_sentry = self.active_sentry + 1
+		if self.active_sentry > #self.sentries then self.active_sentry = 1 end
+		if self.sentries[self.active_sentry].movable then break end
+	end
+end
+
+function Game:see_board()
+	print(inspect(self.board_color))
 end
 
 function Game:play()
 	self:init()
 	self:fill_board()
+	self:update_movable_sentries()
+	self:toggle_active_sentry()
  	while true do  -- Main loop.
+		self:update_movable_sentries()
     		self:handle_input(stats, fall, next_piece)
-    		--lower_piece_at_right_time(stats, fall, next_piece)
-		self:fill_board()
     		self:draw_screen()
 
     		-- Don't poll for input much faster than the display can change.
     		local sec, nsec = 0, 5e6  -- 0.005 seconds.
     		posix.nanosleep(sec, nsec)
   	end
+	--]]
 end
 
 g = Game:new()
